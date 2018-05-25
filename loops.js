51d64b731ffc938bb28dd934cd0a6802f2f989a9
Kelly Lwakatare <devkells00@gmail.com>
2018-05-25 04:00:23 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+  if (i = 1) {
+    console.log("I am 1 strange loop.");
+  }
+  else {
+    console.log("I am ${i} strange loops.");
+  }
+}
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+   }
+   
+  return 'done'
+}
+
